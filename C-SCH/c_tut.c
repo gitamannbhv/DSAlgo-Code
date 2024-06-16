6e0700b73b7d8bf7a7c26d6d1bfffdeb6286d6ff
Aman Anubhav <amannbhv.cswork@gmail.com>
2024-06-16 20:56:52 +0530

    uploaded revisiting c and added progress programs in other folders

---
+++ C-SCH/c_tut.c
@@ -0,0 +1,51 @@
+# include<stdio.h>
+/*
+int main() {
+    int a, b;
+    printf("enter a");
+    scanf("%d", &a);
+
+    printf("enter b");
+    scanf("%d", &b);
+    
+    printf("total sum is : %d", a + b);
+    return 0;
+}
+*/
+
+/*
+int main() {
+    printf("Hello World");
+    return 0;
+}
+*/
+
+//Q1. wap to calc area of square
+
+/*
+int main () {
+    float side;
+    printf("enter the value of side: ");
+    scanf("%f", &side);
+    
+    printf("area of sqr is: %f", side * side);
+    return 0;
+}
+*/
+
+//Q2. wap to calc ar of circle
+/*
+int main () {
+    float rad;
+    float pi = 3.14;
+    printf("enter the value of radius: ");
+    scanf("%f", &rad);
+    
+    printf("area of sqr is: %f", pi * rad * rad);
+    return 0;
+}
+*/
+
+int main () {
+    
+}
---
+++ REVISITNG-C/add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    printf("Enter Number a and b\n");
+    scanf("%d \n %d", &a, &b);
+    printf("sum of the numbers are: %d", a+b);
+    return 0;
+}
