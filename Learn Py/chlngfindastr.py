5c5cae919e661d7507bfbee998774a7ce6cbc026
Aman Anubhav <amannbhv.cswork@gmail.com>
2025-10-30 23:19:56 +0530

    challenge - finding str

--- Learn Py/chlngfindastr.py
+++ Learn Py/chlngfindastr.py
@@ -1,6 +1,9 @@
 '''In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
 
-NOTE: String letters are case-sensitive.'''
+NOTE: String letters are case-sensitive.
+
+The first line of input contains the original string. The next line contains the substring.
+Output the integer number indicating the total number of occurrences of the substring in the original string.'''
 
 def count_substring(string, sub_string):
     cnt=0
