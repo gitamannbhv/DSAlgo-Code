7a75deb93db7b2c9782081b86bf1c49acc72bbd3
Aman Anubhav <amannbhv.cswork@gmail.com>
2025-10-31 23:56:52 +0530

    string validator

--- Learn Py/strvalidator.py
+++ Learn Py/strvalidator.py
@@ -1,4 +1,9 @@
-'''Python has built-in string validation methods for basic data. It can check if a string is composed of alphabetical characters, alphanumeric characters, digits, etc.'''
+'''Python has built-in string validation methods for basic data. It can check if a string is composed of alphabetical characters, alphanumeric characters, digits, etc.
+In the first line, print True if  has any alphanumeric characters. Otherwise, print False.
+In the second line, print True if  has any alphabetical characters. Otherwise, print False.
+In the third line, print True if  has any digits. Otherwise, print False.
+In the fourth line, print True if  has any lowercase characters. Otherwise, print False.
+In the fifth line, print True if  has any uppercase characters. Otherwise, print False.'''
 
 if __name__ == '__main__':
     s = input()
