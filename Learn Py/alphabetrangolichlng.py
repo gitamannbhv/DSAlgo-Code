f9cb45ff0f7d1a598d9bfdfdd4ac1423f3149b06
Aman Anubhav <amannbhv.cswork@gmail.com>
2025-11-10 23:38:25 +0530

    question added rangoli

---
+++ Learn Py/alphabetrangolichlng.py
@@ -0,0 +1,3 @@
+'''You are given an integer, . Your task is to print an alphabet rangoli of size . (Rangoli is a form of Indian folk art based on creation of patterns.)
+
+Different sizes of alphabet rangoli are shown below:'''
